17bd05dc1406a8b82dd8103a838dd83e03d5c784
emujintsogo <ket2146@barnard.edu>
2024-01-14 23:37:11 -0600

    updated script

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("canvas1"); 
 const context = canvas.getContext('2d');
+let cellSize;
 
 const image = new Image();
-image.src = "../images/default.jpg";
 image.onload = function() {
     initialize();
 }
@@ -12,26 +12,18 @@ let inputFile = document.getElementById("inputFile");
 inputFile.onchange = function() {
     const newImage = new Image();
     newImage.src = URL.createObjectURL(inputFile.files[0]);
-
     newImage.onload = function() {
         image.src = newImage.src;
         initialize();
-    } 
-}
+    }
+} 
 
-const sliderX1 = document.getElementById("x1-slider");
-const sliderY1 = document.getElementById("y1-slider");
-const inputLabelX1 = document.getElementById("x1-sliderLabel");
-const inputLabelY1 = document.getElementById("y1-sliderLabel");
-/*
-const resLabel = document.getElementById("resolutionLabel");
-const resSlider = document.getElementById("resolution");
-*/
-sliderX1.addEventListener("change", handleSliderX1);
-sliderY1.addEventListener("change", handleSliderY1);
-//resSlider.addEventListener("change", handleResolution);
+const slider = document.getElementById("y1-slider");
+const sliderLabel = document.getElementById("y1-sliderLabel");
+const fontSize = document.getElementById("fontSize");
 
-let cellSize = 10;
+slider.addEventListener("change", handleControls);
+fontSize.addEventListener("change", handleControls);
 
 class Cell {
     constructor(x, y, symbol, color) {
@@ -45,7 +37,6 @@ class Cell {
         context.fillStyle = this.color;
         context.fillText(this.symbol, this.x, this.y);
     }
-
 }
 
 class AsciiEffect {
@@ -92,7 +83,8 @@ class AsciiEffect {
         else return "";
     }
 
-    #scanImage() {
+    #scanImage(fontSize) {
+        cellSize = fontSize;
         this.imageCellArray = [];
         for(let y = 0; y < this.#height; y += cellSize) {
             for(let x = 0; x < this.#width; x += cellSize) {
@@ -111,64 +103,35 @@ class AsciiEffect {
         }
     }
 
-    #drawAscii(lines, direction) {
-        this.#context.drawImage(image, 0, 0, canvas.width, canvas.height);
-        if(direction == "x1") {
-            for(let x = 0; x<=lines; x++) {
-                this.#context.clearRect(x*cellSize, 0, cellSize, this.#height);
-                for(let y = x; y< this.imageCellArray.length; y+= this.#width/cellSize)
-                    this.imageCellArray[y].draw(this.#context);
-            }
-        } else if(direction == "y1") {
-            for(let y = 0; y<=lines;y++) { 
-                this.#context.clearRect(0, y*cellSize - cellSize, this.#width, cellSize);
-                for(let x = y*(this.#width/cellSize) ;x<y*this.#width/cellSize + this.#width/cellSize;x++) {              
-                    this.imageCellArray[x].draw(this.#context);
-                }
+    #drawAscii(lines) {
+    this.#context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    for(let y=0; y<=lines;y++) { 
+        this.#context.clearRect(0, y*cellSize - cellSize, this.#width, cellSize);
+        for(let x = Math.floor(y*this.#width/cellSize);x<Math.floor(y*this.#width/cellSize + this.#width/cellSize);x++) {              
+            this.imageCellArray[x].draw(this.#context);
             }
         }
-    } 
+    }
     
-    getHeight() {
-        return this.#height;
-    }
-
-    getWidth() {
-        return this.#width;
-    }
-
-    draw( lines, direction) {
-        this.#scanImage();
-        this.#drawAscii(lines, direction); 
+    draw(lines, fontSize) {
+        this.#scanImage(fontSize);
+        this.#drawAscii(lines); 
     } 
-
 }
 
 let effect;
 
-/*function handleResolution() {
-    let cellSize = parseInt(resSlider.value);
-} */
-
-function handleSliderX1() {
-    //handleResolution();
-    sliderX1.max = effect.getWidth()/cellSize;
-    inputLabelX1.innerHTML = "Slider: " + sliderX1.value;
-    effect.draw(parseInt(sliderX1.value), "x1");
-}
-
-
-function handleSliderY1() {
-   // handleResolution();
-    sliderY1.max = effect.getHeight()/cellSize;
-    inputLabelY1.innerHTML = "Slider: " + sliderY1.value;
-    effect.draw( parseInt(sliderY1.value), "y1");
+function handleControls() {
+    const selectedFontSize = parseInt(fontSize.value);
+    slider.max = Math.floor(image.height/selectedFontSize) + 1;
+    context.font = selectedFontSize + "px Verdana";
+    sliderLabel.innerHTML = "Slider: " + slider.value;
+    effect.draw(parseInt(slider.value), selectedFontSize);
 }
 
 function initialize() {
     canvas.width = image.width;
     canvas.height = image.height;
     effect = new AsciiEffect(context, image.width, image.height);
-    handleSliderX1();
-    handleSliderY1();
+    handleControls();
 }
